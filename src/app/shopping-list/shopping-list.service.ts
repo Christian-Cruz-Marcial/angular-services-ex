74376db27c26f9e7b571ce1d15d6a86b95220e06
unknown <c.cruzmarcial2001@gmail.com>
2023-12-06 14:33:06 -0600

    finished video 4 and  5

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 export class ShoppingListService{
-  
+  ingredientChange = new EventEmitter<Ingredient[]>();
+  ingredients: Ingredient[] =[
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ] ;
+
+  getIngredients(){
+    return this.ingredients.slice()
+  }
+  addingIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientChange.emit(this.ingredients.slice());
+  }
+
 }
